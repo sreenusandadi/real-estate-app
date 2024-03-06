e48810043dda22e14c819f4ce4c889e37c330a9b
sandadi Sreenu <sreenu.friends18@gmail.com>
2024-03-06 17:04:40 +0530

    error middleware added

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,10 +1,14 @@
 import User from "../models/user.model.js";
-import bcryptjs from bcryptjs;
+import bcryptjs from "bcryptjs";
 
-export const auth = async (req, res) => {
+export const auth = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
-  await newUser.save();
-  res.status(201).json("user created succefully");
+  try {
+    await newUser.save();
+    res.status(201).json("user created succefully");
+  } catch (error) {
+    next(error);
+  }
 };
