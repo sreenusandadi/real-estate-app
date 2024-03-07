8903c198a65e3e73b413fcbf4eeeb1d4f6f39a4a
sandadi Sreenu <sreenu.friends18@gmail.com>
2024-03-07 17:23:37 +0530

    profile functionality added

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       default:
-        "https://www.google.com/url?sa=i&url=https%3A%2F%2Ficonduck.com%2Ficons%2F112808%2Fuser&psig=AOvVaw0xP1h-fxa5ikTmgH957joe&ust=1709887658211000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCJjRtYri4YQDFQAAAAAdAAAAABAE",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCJoVnuKWv43i_Tti7OWhbBsfeyYDCi7KjYxcyXP--Qg&s",
     },
   },
   { timestamps: true }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { FaSearch } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -30,8 +32,16 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to="sign-in">
-            <li className="text-slate-700 hover:underline">Sign In</li>
+          <Link to="profile">
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                className="rounded-full w-7 h-7 object-cover"
+                alt="Profile"
+              />
+            ) : (
+              <li className="text-slate-700 hover:underline">Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
