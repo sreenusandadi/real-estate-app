54234665989bbdbfdaa98cd7134b5bc68324aeb3
sandadi Sreenu <sreenu.friends18@gmail.com>
2024-03-10 15:26:50 +0530

    create listing server changes fix

---
+++ api/routes/listing.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { createListing } from "../controller/listing.controller.js";
+
+const router = express.Router();
+
+router.post("/create", createListing);
+
+export default router;
